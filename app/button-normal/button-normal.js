38cb76c06786d3386dd726e60964d7a473b7077a
pilichanampe <pilichanampe@hotmail.com>
2020-07-22 16:22:46 -0300

    Naming the html structure

---
+++ app/button-normal/button-normal.js
@@ -0,0 +1,11 @@
+class ButtonNormal extends HTMLElements {
+  constructor() {
+    super();
+
+    loadTemplate("#button-normal", this);
+  };
+   
+  
+}
+
+customElements.define("button-normal", ButtonNormal);
